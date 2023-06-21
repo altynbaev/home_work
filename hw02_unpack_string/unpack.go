92e825473fc34ac54ad7a38c3c5278ef89a5df9f
altynbaev <4ltynbaev@gmail.com>
2023-06-21 16:19:26 +0300

    Implement unpack string func

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,59 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var output strings.Builder
+
+	if len(s) == 0 {
+		return "", nil
+	}
+	digitsMap := map[rune]int{
+		'0': 0,
+		'1': 1,
+		'2': 2,
+		'3': 3,
+		'4': 4,
+		'5': 5,
+		'6': 6,
+		'7': 7,
+		'8': 8,
+		'9': 9,
+	}
+
+	chars := []rune(s)
+
+	_, ok := digitsMap[chars[0]]
+	if ok {
+		return "", ErrInvalidString
+	}
+
+	for i := 1; i < len(s); i++ {
+		prevChar := chars[i-1]
+		digit, ok := digitsMap[chars[i]]
+		if ok {
+			_, ok := digitsMap[prevChar]
+			if ok {
+				return "", ErrInvalidString
+			}
+			if digit == 0 {
+				continue
+			}
+			output.WriteString(strings.Repeat(string(prevChar), digit))
+		} else {
+			_, ok := digitsMap[prevChar]
+			if !ok {
+				output.WriteRune(prevChar)
+			}
+			if i == len(s)-1 {
+				output.WriteRune(chars[i])
+			}
+		}
+	}
+
+	return output.String(), nil
 }
