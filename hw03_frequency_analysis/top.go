4e4c92050e93e85c47fec42f20857b8d2ce1fed8
altynbaev <4ltynbaev@gmail.com>
2023-07-02 17:28:37 +0300

    HW03 Add solution

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(s string) []string {
+	inputSlice := strings.Fields(s)
+
+	wordsMap := map[string]int{}
+	wordsSlice := make([]string, 0, len(inputSlice))
+
+	for _, word := range inputSlice {
+		wordsMap[word]++
+	}
+
+	for word := range wordsMap {
+		wordsSlice = append(wordsSlice, word)
+	}
+
+	sort.Slice(wordsSlice, func(i int, j int) bool {
+		// Условие для лексикографической сортировки при равном количестве слов
+		if wordsMap[wordsSlice[i]] == wordsMap[wordsSlice[j]] {
+			return wordsSlice[i] < wordsSlice[j]
+		}
+		// Условие для сортировки по количеству
+		return wordsMap[wordsSlice[i]] > wordsMap[wordsSlice[j]]
+	})
+
+	var sliceLen int
+	if len(inputSlice) < 10 {
+		sliceLen = len(wordsSlice)
+	} else {
+		sliceLen = 10
+	}
+
+	return wordsSlice[:sliceLen]
 }
