cd00715d367105f64bfbb2a0d880ba455355ba93
altynbaev <4ltynbaev@gmail.com>
2023-07-25 17:40:50 +0300

    Use require eventually test func

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -141,4 +141,30 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("test concurrent run", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := 1
+
+		require.Eventually(t, func() bool {
+			return Run(tasks, workersCount, maxErrorsCount) == nil
+		}, time.Second, time.Millisecond, "the tasks were not completed concurrently")
+	})
 }
