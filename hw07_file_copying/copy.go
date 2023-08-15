55c30737f22247e14b4a170d0164bc1d8d217531
altynbaev <4ltynbaev@gmail.com>
2023-08-16 02:01:40 +0300

    Implement HW07 file copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +15,61 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("open source file error: %w", err)
+	}
+	defer func() {
+		err := sourceFile.Close()
+		if err != nil {
+			fmt.Println(fmt.Errorf("close source file error: %w", err))
+		}
+	}()
+
+	sourceFileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+	sourceFileSize := sourceFileInfo.Size()
+	if sourceFileSize <= offset {
+		return ErrOffsetExceedsFileSize
+	}
+	if offset > 0 {
+		_, err = sourceFile.Seek(offset, 0)
+		if err != nil {
+			return err
+		}
+	}
+
+	bar := pb.Full.Start64(limit)
+	defer bar.Finish()
+	barReader := bar.NewProxyReader(sourceFile)
+
+	targetFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err := targetFile.Close()
+		if err != nil {
+			fmt.Println(fmt.Errorf("close target file error: %w", err))
+		}
+	}()
+
+	if limit != 0 {
+		if limit+offset > sourceFileSize {
+			limit = sourceFileSize - offset
+		}
+		_, err = io.CopyN(targetFile, barReader, limit)
+		if err != nil {
+			return fmt.Errorf("copy file error: %w", err)
+		}
+		return nil
+	}
+	_, err = io.Copy(targetFile, barReader)
+	if err != nil {
+		return fmt.Errorf("copy file error: %w", err)
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,102 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/udhos/equalfile"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		refFile string
+	}{
+		{
+			name: "offset0_limit0",
+			args: args{
+				fromPath: "testdata/input.txt",
+				toPath:   "/tmp/test_offset0_limit0.txt",
+				offset:   0,
+				limit:    0,
+			},
+			wantErr: false,
+			refFile: "testdata/out_offset0_limit0.txt",
+		},
+		{
+			name: "offset0_limit10",
+			args: args{
+				fromPath: "testdata/input.txt",
+				toPath:   "/tmp/test_offset0_limit10.txt",
+				offset:   0,
+				limit:    10,
+			},
+			wantErr: false,
+			refFile: "testdata/out_offset0_limit10.txt",
+		},
+		{
+			name: "offset0_limit1000",
+			args: args{
+				fromPath: "testdata/input.txt",
+				toPath:   "/tmp/test_offset0_limit1000.txt",
+				offset:   0,
+				limit:    1000,
+			},
+			wantErr: false,
+			refFile: "testdata/out_offset0_limit1000.txt",
+		},
+		{
+			name: "offset100_limit1000",
+			args: args{
+				fromPath: "testdata/input.txt",
+				toPath:   "/tmp/test_offset100_limit1000.txt",
+				offset:   100,
+				limit:    1000,
+			},
+			wantErr: false,
+			refFile: "testdata/out_offset100_limit1000.txt",
+		},
+		{
+			name: "offset0_limit10000",
+			args: args{
+				fromPath: "testdata/input.txt",
+				toPath:   "/tmp/test_offset0_limit10000.txt",
+				offset:   0,
+				limit:    10000,
+			},
+			wantErr: false,
+			refFile: "testdata/out_offset0_limit10000.txt",
+		},
+		{
+			name: "offset6000_limit1000",
+			args: args{
+				fromPath: "testdata/input.txt",
+				toPath:   "/tmp/test_offset6000_limit1000.txt",
+				offset:   6000,
+				limit:    1000,
+			},
+			wantErr: false,
+			refFile: "testdata/out_offset6000_limit1000.txt",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Copy(tt.args.fromPath, tt.args.toPath, tt.args.offset, tt.args.limit); (err != nil) != tt.wantErr {
+				t.Errorf("Copy() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			cmp := equalfile.New(nil, equalfile.Options{}) // compare using single mode
+			equal, err := cmp.CompareFile(tt.args.toPath, tt.refFile)
+			require.NoError(t, err)
+			assert.True(t, equal)
+		})
+	}
 }
