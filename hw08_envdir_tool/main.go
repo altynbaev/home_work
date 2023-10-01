402cf4e0960a7c1528efb1c1481453909ab67d2b
altynbaev <4ltynbaev@gmail.com>
2023-10-01 13:18:45 +0300

    Init

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
+
+func FileExists(fileName string) bool {
+	if _, err := os.Stat(fileName); errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	return true
+}
+
 func main() {
-	// Place your code here.
+	envDir := os.Args[1]
+	if envDir == "" {
+		log.Fatalf("Empty path")
+	}
+	if !FileExists(envDir) {
+		log.Fatal("Directory does not exists or not enough permissions")
+	}
+	env, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	RunCmd(os.Args[2:], env)
 }
